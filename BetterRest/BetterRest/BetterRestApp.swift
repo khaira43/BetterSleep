dae998c949c5e52e10039caf32b6a7421eb0c008
Ranbir Singh Khaira <rkhaira1010@gmail.com>
2025-03-22 17:03:20 -0400

    Add files via upload

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ranbir Khaira on 2025-03-21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
